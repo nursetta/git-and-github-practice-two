ed4f1baf7e6b38c37d31603b6bd1e64a733be05e
nursetta <n_ursetta@yahoo.com>
2016-06-20 11:45:31 -0600

    add files

---
+++ javascripts.js
@@ -0,0 +1,11 @@
+// JavaScript to be added 
+window.onload = start;
+function start(){
+document.getElementById('button-yellow').addEventListener('click', addEmail);
+
+}
+
+function addEmail () {
+	prompt('Please enter Email');
+
+}
